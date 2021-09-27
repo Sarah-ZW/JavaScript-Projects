da7d99f5e0343881bc1460f4079cd0afdd2bc4aa
Sarah-ZW <earthclaninfo@gmail.com>
2021-09-26 21:31:16 -0400

    First JavaScript assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("WELCOME!")
+document.write("Hello World <br>"); //typing the words
+var j = "Catan"; // assigning a variable
+document.write(j); //displaying the variable
+document.write("<br>Tomato said \"for the love of cucumber\"<br>"); //using escape key to display ""
+var a = "My favorite game to play is ";
+var c = a+j;
+document.write(a+j); //2 variable concantenation
+document.write("<br> My favorite game to play is " + j + " and you?"); //concantenation
+x = "<br> This is a" + " concantenated string that was assigned to a variable<br>";
+document.write(x);
+var i = "jelly", m = "shitake", t = "spoon"; //assigning mutiple variables at once
+document.write(i);
+var i = i.fontcolor("orange"); //assigning color to a variable
+var m = m.fontcolor("grey");
+var t = t.fontcolor("blue");
+document.write(i,m,t);
+document. write(3+3); //expression
